d0094866b7596d7bc275a9e6fabaa0b22382e6fb
Muhammad Shahid <88853986+muhammadShahidk@users.noreply.github.com>
2022-12-20 23:13:27 +0500

    Added Filters on form input
    name input filed will have ABC only
    the phone will have a number only
    and other

--- src/wwwroot/js/Employee/emp.js
+++ src/wwwroot/js/Employee/emp.js
@@ -1,1 +1,45 @@
 ﻿console.log("hello");
+
+let inputs = document.querySelectorAll("input");
+
+for (var i = 0; i < 3; i++) {
+   
+
+    if (i < 2) {
+        inputs[i].addEventListener("keydown", (e) => {
+            console.log(e);
+
+            if (e.keyCode > 95 && e.keyCode <= 105) {
+                e.preventDefault();
+                alert(e.keyCode);
+            }
+        });
+
+    }
+    else {
+        inputs[i].addEventListener("keydown", (e) => {
+            console.log(e);
+
+            if (e.keyCode > 65 && e.keyCode <= 90) {
+                e.preventDefault();
+                alert(e.keyCode);
+            }
+        });
+    }
+}
+
+
+//--? Extera Work
+//inputs.forEach((item) => {
+//    item.addEventListener("keydown", (e) => {
+//        console.log(e);
+
+//        if (e.keyCode > 95 && e.keyCode <= 105) {
+//            e.preventDefault();
+//            alert(e.keyCode);
+//        }
+//    })
+//});
+
+console.log(inputs[2].parentElement.children[0].innerHTML)
+console.log(inputs);
